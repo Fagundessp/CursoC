4a1e735b3e2e2dcd083f0a4f08ee0907d08a99e5
Fagundessp <143969314+Fagundessp@users.noreply.github.com>
2023-09-03 19:16:50 -0300

    Update Contador de batidas.c

--- Contador de batidas.c
+++ Contador de batidas.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main () {
-float idade, batidas, segundo, hora, dia, anos;
- segundo = 1 ;
- hora = 60;
- dia = 24;
+float batidas, minutos, segundo, hora, anos;
+ segundo = 60;
+ minutos = 60
+ hora = 24;
  anos = 365,25;
  
 printf ("insira sua idade:\n");
 scanf("%f", &idade);
 
-batidas = segundo * hora * dia * anos;
+batidas = segundo * minutos * horas * anos;
 
 printf ("quantidade de batidas: %2.f\n", batidas);
 
